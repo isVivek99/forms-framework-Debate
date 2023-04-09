9b0549168d6a0231e6cae7fcb485c8edb6333038
vivek lokhande <vivek2811999@gmail.com>
2023-04-09 13:46:00 +0530

    added post mutation

--- 03-pespa/blog-tutorial/app/routes/posts.tsx
+++ 03-pespa/blog-tutorial/app/routes/posts.tsx
@@ -1,79 +1,56 @@
 import React from "react";
-import { useForm, Resolver } from "react-hook-form";
-import "./blog.css";
+import {
+  useLocation,
+  Link,
+  useLoaderData,
+  useFetcher,
+  Form,
+} from "@remix-run/react";
+import { ActionArgs, json, LinksFunction } from "@remix-run/node";
+import invariant from "tiny-invariant";
+import postStylesheet from "./blog.css";
+import * as db from "../db";
 
 type Post = { id: string; title: string; desc: string; content: string };
 
-type FormValues = {
-  title: string;
-  desc: string;
-  content: string;
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: postStylesheet }];
 };
 
-const resolver: Resolver<FormValues> = async (values) => {
-  console.log(values);
+export async function loader() {
+  const posts = await db.getPosts();
+  return json({ posts });
+}
 
-  return {
-    values: values.title ? values : {},
-    errors: !values.title
-      ? {
-          title: {
-            type: "required",
-            message: "This is required.",
-          },
-        }
-      : !values.desc
-      ? {
-          desc: {
-            type: "required",
-            message: "This is required.",
-          },
-        }
-      : !values.content
-      ? {
-          content: {
-            type: "required",
-            message: "This is required.",
-          },
-        }
-      : {},
-  };
-};
-let renderCount = 0;
-const Posts = () => {
-  const [posts, setPosts] = React.useState<Array<Post>>([]);
-  const [statuses, setStatuses] = React.useState<{
-    loadingPosts: "idle" | "loading";
-    creatingPost: "idle" | "loading";
-  }>({
-    loadingPosts: "loading",
-    creatingPost: "idle",
-  });
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({ resolver });
+export async function action({ request }: ActionArgs) {
+  let formData = await request.formData();
+  const title = formData.get("title");
+  const desc = formData.get("title");
+  const content = formData.get("title");
+  invariant(typeof title === "string", "title must be a string");
+  invariant(typeof desc === "string", "desc must be a string");
+  invariant(typeof content === "string", "content must be a string");
+  console.log({ title, desc, content });
+  await db.createPost({ title, desc, content });
 
-  renderCount++;
+  return null;
+}
 
-  // initial load of posts
-  React.useEffect(() => {
-    fetch("http://localhost:4131/api/posts")
-      .then((res) => res.json())
-      .then(({ posts }) => {
-        setPosts(posts);
-        setStatuses((old) => ({ ...old, loadingTodos: "idle" }));
-      });
-  }, []);
+export default function Posts() {
+  const data = useLoaderData() as { posts: Array<Post> };
+
+  const createFetcher = useFetcher();
+  const clearFetcher = useFetcher();
+  const toggleAllFetcher = useFetcher();
+  const createFormRef = React.useRef<HTMLFormElement>(null);
+  const location = useLocation();
 
   return (
     <div className=" m-auto">
-      renderCount:{renderCount}
       <div className="flex">
         <div className="flex-basis-1-3">
-          <ul hidden={!posts.length}>
-            {posts.map((post, i) => (
+          <ul hidden={!data.posts.length}>
+            {data.posts.map((post, i) => (
               <li key={i}>
                 <p className="bold my-1">{post.title}</p>
                 <p className="bold my-1">{post.desc}</p>
@@ -87,60 +64,29 @@ const Posts = () => {
           <div className="border-color-brown border-b-2">
             <h1>My Blog</h1>
           </div>
-          <form
-            onSubmit={handleSubmit((data, e) => {
-              const { title, desc, content } = data;
-              e?.target.reset();
-
-              setPosts([
-                ...posts,
-                { title, desc, content, id: Math.random().toFixed(2) },
-              ]);
-
-              setStatuses((old) => ({ ...old, creatingPost: "loading" }));
-              fetch(`http://localhost:4131/api/posts`, {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ title, desc, content }),
-              })
-                .then((res) => res.json())
-                .then((data) => {
-                  setPosts((prev) => {
-                    prev[prev.length - 1] = data.post;
-                    console.log(posts);
-                    return prev;
-                  });
-
-                  setStatuses((old) => ({ ...old, creatingPost: "idle" }));
-                });
-            })}
-          >
+          <Form method="post">
             <div className="flex flex-col">
               <label htmlFor="input-1">
                 <h3>title</h3>
               </label>
               <input
+                type="text"
                 id="input-1"
-                className={`w-100 ${
-                  errors.title?.message ? "input-error" : ""
-                }`}
-                data-pending={statuses.creatingPost === "loading"}
-                {...register("title", { required: "this field is required." })}
+                className="w-100"
+                autoFocus
+                //data-pending={statuses.creatingPost === "loading"}
+                name="title"
               />
-
-              <p className="error">{errors.title?.message}</p>
               <label htmlFor="input-2">
                 <h3>desc</h3>
               </label>
               <input
+                type="text"
                 id="input-2"
-                className={`w-100 ${errors.desc?.message ? "input-error" : ""}`}
-                data-pending={statuses.creatingPost === "loading"}
-                {...register("desc", {
-                  required: "this field is required.",
-                })}
+                className="w-100"
+                name="desc"
+                //data-pending={statuses.creatingPost === "loading"}
               />
-              <p className="error">{errors.desc?.message}</p>
               <label htmlFor="input-3">
                 <h3>content</h3>
               </label>
@@ -148,29 +94,21 @@ const Posts = () => {
                 id="input-3"
                 cols={30}
                 rows={10}
-                className={`w-100 ${
-                  errors.content?.message ? "input-error" : ""
-                }`}
-                {...register("content", {
-                  required: "this filed is also required.",
-                })}
-                data-pending={statuses.creatingPost === "loading"}
+                name="content"
+                //data-pending={statuses.creatingPost === "loading"}
               ></textarea>
-              <p className="error">{errors.content?.message}</p>
             </div>
             <div className="flex justify-end">
               <button
                 type="submit"
-                disabled={statuses.creatingPost === "loading"}
+                // disabled={statuses.creatingPost === "loading"}
               >
                 create post
               </button>
             </div>
-          </form>
+          </Form>
         </div>
       </div>
     </div>
   );
-};
-
-export default Posts;
+}
